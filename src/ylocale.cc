8867228022c4ce0f4cd157be468ce5fc58538b0e
massel <>
2001-07-24 14:05:24 +0000

    locale related stuff

---
+++ src/ylocale.cc
@@ -0,0 +1,100 @@
+/*
+ *  IceWM - C++ wrapper for locale/unicode conversion
+ *  Copyright (C) 2001 The Authors of IceWM
+ *
+ *  Released under terms of the GNU Library General Public License
+ *
+ *  2001/07/21: Mathias Hasselmann <mathias.hasselmann@gmx.net>
+ *	- initial revision
+ */
+
+#include "config.h"
+#include "ylocale.h"
+
+#include "default.h"
+#include "base.h"
+
+#include "intl.h"
+
+#ifdef CONFIG_I18N
+#include <errno.h>
+#include <langinfo.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wchar.h>
+
+#include <X11/Xlib.h>
+#endif
+
+YLocale * YLocale::locale(NULL);
+
+YLocale::YLocale(char const * localeName) {
+#ifdef CONFIG_I18N
+    locale = this;
+
+    if (NULL == setlocale(LC_ALL, localeName))// || False == XSupportsLocale())
+	setlocale(LC_ALL, "C");
+
+    multiByte = (MB_CUR_MAX > 1);
+
+    msg("I18N: locale: %s, MB_CUR_MAX: %d, multibyte: %d, codeset: %s",
+    	setlocale(LC_ALL, NULL), MB_CUR_MAX, multiByte, QUERY_CODESET);
+
+    char const * lcs(QUERY_CODESET);
+    char const * ucs("WCHAR_T//TRANSLIT");
+
+    if ((iconv_t) -1 == (toUnicode = iconv_open(ucs, lcs)))
+	die(1, _("Can't establish %s to %s conversion"), lcs, ucs);
+
+    ucs = "WCHAR_T";
+    lcs = strJoin(lcs, "//TRANSLIT", NULL);
+    if ((iconv_t) -1 == (toLocale = iconv_open(lcs, ucs)))
+	die(1, _("Can't establish %s to %s conversion"), lcs, ucs);
+
+    delete[] lcs;
+#endif
+
+#ifdef ENABLE_NLS
+    bindtextdomain(PACKAGE, LOCDIR);
+    textdomain(PACKAGE);
+#endif
+}
+
+YLocale::~YLocale() {
+#ifdef CONFIG_I18N
+    locale = NULL;
+
+    if ((iconv_t) -1 != toUnicode) iconv_close(toUnicode);
+    if ((iconv_t) -1 != toLocale) iconv_close(toLocale);
+#endif    
+}
+
+#ifdef CONFIG_I18N
+/*
+lchar_t * YLocale::localeString(uchar_t const * uStr) {
+    lchar_t * lStr(NULL);
+    
+    return lStr;
+}
+*/
+
+uchar_t * YLocale::unicodeString(lchar_t const * lStr, size_t const lLen,
+    				 size_t & uLen) {
+    if (NULL == lStr || NULL == locale)
+	return NULL;
+
+    uchar_t * uStr(new uchar_t[lLen + 1]);
+    char * inbuf((char *) lStr), * outbuf((char *) uStr);
+    size_t inlen(lLen), outlen(4 * lLen);
+
+    if (0 > (int) iconv(locale->toUnicode, &inbuf, &inlen, &outbuf, &outlen))
+	warn(_("Invalid multibyte string \"%s\": %s"), lStr, strerror(errno));
+
+    *((uchar_t *) outbuf) = 0;
+    uLen = ((uchar_t *) outbuf) - uStr;
+    
+    return uStr;
+}
+
+#endif
---
+++ src/ystring.h
@@ -0,0 +1,134 @@
+/*
+ *  IceWM - string classes
+ *  Copyright (C) 2001 The Authors of IceWM
+ *
+ *  Release under terms of the GNU Library General Public License
+ */
+
+#ifndef __YSTRING_H
+#define __YSTRING_H
+
+#include "ylocale.h"
+
+template <class DataType> class YString {
+public:
+    typedef ::size_t size_t;
+    typedef DataType data_t;
+
+    YString(data_t const * str) {
+	set(str);
+    }
+    
+    YString(data_t const * str, size_t len) {
+	set(str, len);
+    }
+    
+    virtual ~YString() {
+	delete[] fData;
+    }
+
+    void set(data_t const * str) {
+	set(str, length(str));
+    }
+
+    void set(data_t const * str, size_t len) {
+	delete[] fData;
+
+	fSize = (fLength = len) + 1;
+	fData = new data_t[fSize];
+
+	::memcpy(fData, str, sizeof(data_t) * fSize);
+    }
+
+    void set(size_t index, data_t const & value) {
+	size_t const size(index + 1);
+
+	if (size > fSize) {
+	    data_t * data(new data_t[size]);
+	    ::memcpy(data, fData, fSize);
+	    ::memset(data + fSize, 0, (size - fSize - 1) * sizeof(data_t));
+
+	    delete[] fData;
+
+	    fData = data;
+	    fLength = index;
+	    fSize = size;
+	}
+
+	fData[index] = value;
+    }
+    
+    data_t const * cStr() {
+	set(fLength, 0);
+	return fData;
+    }
+    
+    data_t const & get(size_t index) const { 
+	return (index < fSize ? fData[index] : 0);
+    }
+
+    data_t const & operator[](size_t index) const {  get(index); }
+    data_t const * data() const { return fData; }
+    size_t length() const { return fLength; }
+    size_t size() const { return fSize; }
+
+    static size_t length(data_t const * str) {
+	if (NULL == str) return 0;
+
+	size_t length(0);
+	while (*str++) ++length;
+	return length;
+    }
+
+    static size_t size(data_t const * str) {
+	return length(str) + 1;
+    }
+
+protected:
+    void assign(data_t * data, size_t length, size_t size) {
+	if (data != fData) {
+	    delete fData;
+	    fData = data;
+	}
+	
+	fLength = length;
+	fSize = size;
+    }
+
+protected:
+    YString(): fData(NULL), fLength(0), fSize(0) {}
+
+private:
+    data_t * fData;
+    size_t fLength, fSize;
+};
+
+class YUnicodeString : public YString<uchar_t> {
+public:
+    YUnicodeString(uchar_t const * str):
+	YString<uchar_t>(str) {}
+    YUnicodeString(uchar_t const * str, size_t len):
+	YString<uchar_t>(str, len) {}
+    YUnicodeString(lchar_t const * lstr):
+	YString<uchar_t>() {
+	size_t ulen(0);
+	uchar_t * ustr(YLocale::unicodeString(lstr, strlen(lstr), ulen));
+	assign(ustr, ulen, ulen + 1);
+    }
+    YUnicodeString(lchar_t const * lstr, size_t llen):
+	YString<uchar_t>() {
+	size_t ulen(0);
+	uchar_t * ustr(YLocale::unicodeString(lstr, llen, ulen));
+	assign(ustr, ulen, ulen + 1);
+    }
+};
+
+class YLocaleString : public YString<lchar_t> {
+public:
+    YLocaleString(lchar_t const * str):
+	YString<lchar_t>(str) {}
+    YLocaleString(lchar_t const * str, size_t len):
+	YString<lchar_t>(str, len) {}
+};
+
+#endif
