d9bf62947520dbed995addffc423e2b3601b66c4
massel <>
2001-07-29 23:43:12 +0000

    autoconf 2.50 compliance

--- acconfig.h
+++ acconfig.h
@@ -1,3 +1,13 @@
+/* define if have old kstat (on solaris only?) */
 #undef HAVE_OLD_KSTAT
+
+/* the locale dependent property describing the codeset (langinfo.h) */
+#undef HAVE_CODESET
+#undef HAVE__NL_CTYPE_CODESET_NAME
+#undef HAVE__NL_MESSAGES_CODESET
+
+/* define how to query the current locale's codeset */
 #undef QUERY_CODESET
+
+/* define when iconv is imported from GNU libiconv */
 #undef CONFIG_LIBICONV
