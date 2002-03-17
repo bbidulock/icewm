7957ab0869a8e9914959a30e9a3ea22cb89fa0be
gallium <gallium>
2002-03-17 12:24:48 +0000

    bugfix: Fix compilation under cygwin or when HAVE_BASENAME is defined

--- src/misc.cc
+++ src/misc.cc
@@ -454,7 +454,7 @@ unsigned strTokens(const char * str, const char * delim) {
 }
 
 #ifndef HAVE_BASENAME
-const * basename(char const *path) {
+char* basename(char *path) {
     char * base = ::strrchr(path, DIR_DELIMINATOR);
     return (base ? base + 1 : path);
 }
