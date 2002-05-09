f64fef641a750ebad159521617e3c43a00bf4b39
captnmark <captnmark>
2002-05-09 20:03:23 +0000

    almost release

--- utils/release.sh
+++ utils/release.sh
@@ -41,13 +41,10 @@ rm config.{cache,log,status}
 
 RELEASE=`sed -ne 's/^%define\>[[:space:]]*release\>[[:space:]]*\<\(.*\)$/\1/p'\
 	< icewm.spec`
-TARBALL="icewm-$VERSION.tar"
+TARBALL="icewm-$VERSION.src.tar"
 
 popd
 echo Building tarball $TARBALL
 tar -cf $TARBALL --exclude=CVS $DISTDIR
 gzip -9 < $TARBALL > "$TARBALL.gz"
-bzip2 -9 < $TARBALL > "$TARBALL.bz2"
 cp -v "$TARBALL.bz2" "$HOME/rpm/SOURCES/"
-ln -sf "$TARBALL.gz" "icewm-$VERSION.tar.gz"
-ln -sf "$TARBALL.bz2" "icewm-$VERSION.tar.bz2"
