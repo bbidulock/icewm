46d6dbf9bee6a46932b92238d0fd8c3123972de7
captnmark <>
2003-01-02 20:14:51 +0000

    fix flashing after blinking include XftCompat

--- src/ypaint.h
+++ src/ypaint.h
@@ -14,6 +14,7 @@
 
 #ifdef CONFIG_XFREETYPE //------------------------------------------------------
 #include <X11/Xft/Xft.h>
+#include <X11/Xft/XftCompat.h>
 #define INIT_XFREETYPE(Member, Value) , Member(Value)
 #else
 #define INIT_XFREETYPE(Member, Value)
