02ec2af44d83bada8b10d59a37bf2cfd1e531e81
captnmark <captnmark>
2003-05-25 07:53:50 +0000

    reduce the window for alt+tab getting stuck (possibly to 0)

--- src/yapp.cc
+++ src/yapp.cc
@@ -730,9 +730,6 @@ int YApplication::mainLoop() {
                     }
                 }
             }
-            if (xev.type == KeyPress) {
-                XAllowEvents(app->display(), ReplayKeyboard, CurrentTime);
-            }
             gettimeofday(&curtime, 0);
             difftime.tv_sec = curtime.tv_sec - idletime.tv_sec;
             difftime.tv_usec = curtime.tv_usec - idletime.tv_usec;
