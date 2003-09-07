3415e698132ab1e9b6e1abcd958e5a2467c34d4b
captnmark <captnmark>
2003-09-07 18:51:15 +0000

    add code for scaling with imlib

--- src/icewmbg_prefs.h
+++ src/icewmbg_prefs.h
@@ -4,6 +4,7 @@ XSV(const char *, DesktopBackgroundColor, "rgb:00/20/40")
 XSV(const char *, DesktopBackgroundPixmap, 0)
 XSV(const char *, DesktopTransparencyColor, 0)
 XSV(const char *, DesktopTransparencyPixmap, 0)
+XIV(bool, desktopBackgroundScaled, false)
 XIV(bool, centerBackground, false)
 XIV(bool, supportSemitransparency, true)
 
@@ -12,6 +13,7 @@ void addBgImage(const char *name, const char *value);
 cfoption icewmbg_prefs[] = {
     OBV("DesktopBackgroundCenter",              &centerBackground,              "Display desktop background centered and not tiled"),
     OBV("SupportSemitransparency",              &supportSemitransparency,       "Support for semitransparent terminals like Eterm or gnome-terminal"),
+    OBV("DesktopBackgroundScaled",              &desktopBackgroundScaled,        "Desktop background scaled to full screen"),
     OSV("DesktopBackgroundColor",               &DesktopBackgroundColor,        "Desktop background color"),
     OSV("DesktopBackgroundImage",               &DesktopBackgroundPixmap,       "Desktop background image"),
     OSV("DesktopTransparencyColor",             &DesktopTransparencyColor,      "Color to announce for semi-transparent windows"),
