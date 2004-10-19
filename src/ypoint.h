b9077d006006c31f268fe76797f06d7b2b88fbc5
captnmark <captnmark>
2004-10-19 19:42:36 +0000

    more taskbar layout fixes

--- src/ypoint.h
+++ src/ypoint.h
@@ -12,26 +12,31 @@ public:
     int x() const { return fX; }
     int y() const { return fY; }
 
+#if 0
     void setX(int x) { fX = x; }
     void setY(int y) { fY = y; }
 
     void setPos(int x, int y) { fX = x; fY = y; }
 
     void setPoint(const YPoint &p) { fX = p.x(); fY = p.y(); }
+#endif
 
     bool equals(const YPoint &p) {
         return ((fX == p.x()) &&
                 (fY == p.y())) ? true : false;
     }
 
+#if 0
     friend YPoint operator+(const YPoint &a, const YPoint &b);
     friend YPoint operator-(const YPoint &a, const YPoint &b);
     friend YPoint operator-(const YPoint &a);
+#endif
 
 private:
     int fX, fY;
 };
 
+#if 0
 inline YPoint operator+(const YPoint &a, const YPoint &b) {
     return YPoint(a.fX + b.fX, a.fY + b.fX);
 }
@@ -43,5 +48,6 @@ inline YPoint operator-(const YPoint &a, const YPoint &b) {
 inline YPoint operator-(const YPoint &a) {
     return YPoint(-a.fX, -a.fY);
 }
+#endif
 
 #endif
