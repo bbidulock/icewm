4f098fbea48654c59366c273b140812f5e7d1dd7
captnmark <captnmark>
2005-03-06 05:30:31 +0000

    more image cleanups

---
+++ src/ypixmap.h
@@ -0,0 +1,54 @@
+#ifndef __YPIXMAP_H
+#define __YPIXMAP_H
+
+#include "ref.h"
+#include "ylib.h"
+
+class YPixmap: public virtual refcounted {
+public:
+    static ref<YPixmap> create(int w, int h, bool mask = false);
+    static ref<YPixmap> load(const char *filename);
+//    YPixmap(YPixmap const &pixmap);
+#ifdef CONFIG_ANTIALIASING
+    YPixmap(YPixbuf & pixbuf);
+#endif
+
+    YPixmap(char const * fileName);
+#if 0
+    YPixmap(char const * fileName, int w, int h);
+#endif
+    YPixmap(int w, int h, bool mask = false);
+    YPixmap(Pixmap pixmap, Pixmap mask, int w, int h);
+#ifdef CONFIG_IMLIB
+    YPixmap(Pixmap pixmap, Pixmap mask, int w, int h, int wScaled, int hScaled);
+    void scaleImage(Pixmap pixmap, Pixmap mask, int x, int y, int w, int h, int nw, int nh);
+#endif
+    ~YPixmap();
+
+private:
+    YPixmap(const ref<YPixmap> &pixmap, int newWidth, int newHeight);
+public:
+    ref<YPixmap> scale(int width, int height);
+    static ref<YPixmap> scale(ref<YPixmap> source, int width, int height);
+
+    Pixmap pixmap() const { return fPixmap; }
+    Pixmap mask() const { return fMask; }
+    int width() const { return fWidth; }
+    int height() const { return fHeight; }
+    
+    bool valid() const { return (fPixmap != None); }
+
+    void replicate(bool horiz, bool copyMask);
+
+    static Pixmap createPixmap(int w, int h);
+    static Pixmap createPixmap(int w, int h, int depth);
+    static Pixmap createMask(int w, int h);
+
+private:
+    Pixmap fPixmap;
+    Pixmap fMask;
+    unsigned int fWidth, fHeight;
+    bool fOwned;
+};
+
+#endif
