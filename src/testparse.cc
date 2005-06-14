007656eebf6dc01f793928593ba8924c728905a0
captnmark <>
2005-06-14 19:12:11 +0000

    new config parser (test code)

---
+++ src/testparse.cc
@@ -0,0 +1,103 @@
+#include "yparse.h"
+
+#include <stdio.h>
+#include <unistd.h>
+
+const char *ApplicationName = "testparse";
+
+void dumpAttributes(ref<YAttribute> node) {
+    while (node != null) {
+        printf(" %s='%s'",
+               cstring(node->name()).c_str(),
+               cstring(node->getValue()).c_str());
+
+        node = node->next();
+    }
+}
+
+void dumpNodes(ref<YNode> node, int level) {
+    int i;
+
+    while (node != null) {
+        ref<YElement> e = node->toElement();
+        if (e != null) {
+            if (e->firstChild() == null) {
+                for (i = 0; i < level; i++)
+                    printf("    ");
+                printf("%s", cstring(e->name()).c_str());
+                dumpAttributes(e->firstAttribute());
+                printf(";\n");
+            } else {
+                for (i = 0; i < level; i++)
+                    printf("    ");
+                printf("%s", cstring(e->name()).c_str());
+                dumpAttributes(e->firstAttribute());
+                printf(" {\n");
+                dumpNodes(e->firstChild(), level + 1);
+                for (i = 0; i < level; i++)
+                    printf("    ");
+                printf("}\n");
+            }
+        } else {
+            ref<YText> t = node->toText();
+            if (t != null) {
+                for (i = 0; i < level; i++)
+                    printf("    ");
+                printf("'%s';\n", cstring(t->text()).c_str());
+            }
+        }
+
+        node = node->next();
+    }
+
+}
+void test(const char *test) {
+    int len = strlen(test);
+    char *buf = new char[len];
+    memcpy(buf, test, len + 1);
+    YParseResult res;
+    ref<YDocument> doc = YDocument::parse(buf, len, res);
+
+    printf("---\n");
+    if (doc != null)
+        dumpNodes(doc->firstChild(), 0);
+    else {
+        msg("error at %d:%d\n", res.row, res.col);
+    }
+}
+
+int main(int argc, char **argv) {
+
+    if (argc > 1) {
+        static char buf[2 * 1024 * 1024];
+        int len = read(0, buf, sizeof(buf));
+        YParseResult res;
+        ref<YDocument> doc = YDocument::parse(buf, len, res);
+        if (doc != null) {
+            msg("ok");
+            //dumpNodes(doc->firstChild(), 0);
+        } else {
+            msg("error at %d:%d\n", res.row, res.col);
+        }
+    } else {
+        test("test;");
+        test("test n='1';");
+        test("test n='1' b='true';");
+        test("test n='1' b='true' { baz; }");
+        test("test ; foo ; ");
+        test("test {}");
+        test("test { 'foo'; \"bar\"; }");
+        test("test { foo; }");
+        test("test; foo; bar;");
+        test("a{b{c{d{e;}}}}");
+        test("a{");
+        test("b");
+        test("b ( {} )");
+        test("d a=");
+        test("d a='");
+        test("'");
+        test("'\\");
+    }
+//    test("test xml:='www.w3.org/xml';");
+//    test("test (foo='a') {}");
+}
---
+++ src/yparse.h
@@ -0,0 +1,138 @@
+#ifndef __YPARSE_H
+#define __YPARSE_H
+
+#include "mstring.h"
+
+class YNode;
+class YDocument;
+class YElement;
+class YAttribute;
+class YText;
+
+struct YParseResult {
+    int row;
+    int col;
+};
+
+class YDocument: public refcounted {
+public:
+    YDocument();
+    ~YDocument();
+
+    ref<YElement> createElement(ustring element, ustring ns);
+    ref<YAttribute> createAttribute(ustring attribute, ustring ns);
+    ref<YAttribute> createAttribute(ustring attribute);
+    ref<YText> createText(ustring text);
+
+    void addChild(ref<YNode> child);
+    void removeChild(ref<YNode> child);
+    ref<YNode> firstChild() { return fFirstChild; }
+    ref<YNode> lastChild() { return fLastChild; }
+
+    static ref<YDocument> parse(char *buf, int len, YParseResult &res);
+private:
+    ref<YNode> fFirstChild;
+    ref<YNode> fLastChild;
+
+    void freeNode(ref<YNode> &n);
+    void freeNodes(ref<YNode> n);
+};
+
+class YNode: public refcounted {
+public:
+    virtual ~YNode();
+
+    enum NodeType {
+        Document,
+        Element,
+        Attribute,
+        Text
+    };
+    NodeType getType() { return fNodeType; }
+
+    virtual ref<YDocument> toDocument() { return null; }
+    virtual ref<YElement> toElement() { return null; }
+    virtual ref<YAttribute> toAttribute() { return null; }
+    virtual ref<YText> toText() { return null; }
+
+    void addChild(ref<YNode> child);
+    void removeChild(ref<YNode> child);
+    ref<YNode> firstChild() { return fFirstChild; }
+    ref<YNode> lastChild() { return fLastChild; }
+    ref<YNode> parent() { return fParent; }
+
+    ref<YNode> next() { return fNext; }
+    ref<YNode> prev() { return fPrev; }
+private:
+    friend class YDocument;
+    friend class YElement;
+    friend class YAttribute;
+    friend class YText;
+
+    YNode(NodeType nodeType): fNodeType(nodeType) {}
+
+    NodeType fNodeType;
+
+    ref<YNode> fParent;
+    ref<YNode> fNext;
+    ref<YNode> fPrev;
+    ref<YNode> fFirstChild;
+    ref<YNode> fLastChild;
+};
+
+class YElement: public YNode {
+    friend class YDocument;
+    friend class YAttribute;
+
+    YElement(ustring name, ustring ns): YNode(Element), fName(name), fNamespace(ns) {}
+public:
+    void setAttribute(ustring name, ustring ns, ustring value);
+//    void setAttribute(ustring name, ustring value);
+//    void addAttribute(ref<YAttribute> attr);
+    void removeAttribute(ustring name, ustring ns);
+
+    virtual ref<YElement> toElement() { return ref<YElement>(this); }
+
+    ustring name() { return fName; }
+    ref<YNode> firstAttribute() { return fFirstAttribute; }
+
+private:
+    ustring fName;
+    ustring fNamespace;
+
+    ref<YAttribute> fFirstAttribute;
+    ref<YAttribute> fLastAttribute;
+};
+
+class YAttribute: public YNode {
+    friend class YDocument;
+    friend class YElement;
+
+    YAttribute(ustring name, ustring ns, ustring value): YNode(Attribute), fName(name), fNamespace(ns), fValue(value) {};
+public:
+    void setValue(ustring value) { fValue = value; }
+    ustring getValue() { return fValue; }
+
+    ustring name() { return fName; }
+
+    virtual ref<YAttribute> toAttribute() { return ref<YAttribute>(this); }
+
+private:
+    ustring fName;
+    ustring fNamespace;
+    ustring fValue;
+};
+
+class YText: public YNode {
+public:
+    YText(): YNode(Text), fText(null) {};
+    YText(ustring text): YNode(Text), fText(text) {};
+
+    virtual ref<YText> toText() { return ref<YText>(this); }
+
+    ustring text() { return fText; }
+private:
+    ustring fText;
+};
+
+#endif
