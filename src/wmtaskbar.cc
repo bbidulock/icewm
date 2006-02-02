f189d99b32f6be20517f85e4a123e7335fd07d80
captnmark <>
2006-02-02 21:26:51 +0000

    better fix

--- src/wmtaskbar.cc
+++ src/wmtaskbar.cc
@@ -184,7 +184,7 @@ YFrameClient(aParent, 0) INIT_GRADIENT(fGradient, NULL)
                     WinHintsSkipTaskBar);
 
     setWinWorkspaceHint(0);
-    setWinLayerHint(taskBarKeepBelow ? WinLayerBelow : WinLayerAboveDock);
+    setWinLayerHint((taskBarAutoHide || fIsCollapsed) ? WinLayerAboveDock : taskBarKeepBelow ? WinLayerBelow : WinLayerDock);
 
     {
         XWMHints wmh;
