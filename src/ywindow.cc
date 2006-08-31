4a28e34d94b68aa206507384aefc5ff5a5add047
captnmark <captnmark>
2006-08-31 21:30:59 +0000

    shut up gcc strict aliasing

--- src/ywindow.cc
+++ src/ywindow.cc
@@ -1416,7 +1416,7 @@ void YWindow::handleXdnd(const XClientMessageEvent &message) {
             YWindow *win;
 
             if (XFindContext(xapp->display(), XdndDropTarget, windowContext,
-                             (XPointer *)&win) == 0)
+                             (XPointer *)(void *)&win) == 0)
                 win->handleDNDLeave();
             XdndDropTarget = None;
         }
@@ -1457,27 +1457,40 @@ void YWindow::handleXdnd(const XClientMessageEvent &message) {
 
         if (target != XdndDropTarget) {
             if (XdndDropTarget) {
-                YWindow *win;
+                union {
+                    YWindow *ptr;
+                    XPointer xptr;
+                } win;
 
                 if (XFindContext(xapp->display(), XdndDropTarget, windowContext,
-                                 (XPointer *)&win) == 0)
-                    win->handleDNDLeave();
+                                 &win.xptr) == 0)
+                    win.ptr->handleDNDLeave();
             }
             XdndDropTarget = target;
             if (XdndDropTarget) {
-                YWindow *win;
+                union {
+                    YWindow *ptr;
+                    XPointer xptr;
+                } win;
 
                 if (XFindContext(xapp->display(), XdndDropTarget, windowContext,
-                                 (XPointer *)&win) == 0)
+                                 &win.xptr) == 0)
                 {
-                    win->handleDNDEnter();
-                    pwin = win;
+                    win.ptr->handleDNDEnter();
+                    pwin = win.ptr;
                 }
             }
         }
         if (pwin == 0 && XdndDropTarget) { // !!! optimize this
+            union {
+                YWindow *ptr;
+                XPointer xptr;
+            } win;
             if (XFindContext(xapp->display(), XdndDropTarget, windowContext,
-                             (XPointer *)&pwin) != 0)
+                             &win.xptr) == 0)
+            {
+                pwin = win.ptr;
+            } else
                 pwin = 0;
         }
         if (pwin)
