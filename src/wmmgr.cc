8a52ff6d75d7717aaab62abebe860395733ca45d
captnmark <>
2006-09-10 18:29:29 +0000

    merged changes up to 1.2.28

--- src/wmmgr.cc
+++ src/wmmgr.cc
@@ -1329,6 +1329,7 @@ YFrameWindow *YWindowManager::manageClient(Window win, bool mapClient) {
     int ch = 1;
     bool canManualPlace = false;
     bool doActivate = (wmState() == YWindowManager::wmRUNNING);
+    bool requestFocus = true;
 
     MSG(("managing window 0x%lX", win));
     frame = findFrame(win);
@@ -1406,7 +1407,7 @@ YFrameWindow *YWindowManager::manageClient(Window win, bool mapClient) {
         doActivate = false;
     }
 
-    frame->doManage(client, doActivate);
+    frame->doManage(client, doActivate, requestFocus);
     MSG(("initial geometry 3 (%d:%d %dx%d)",
          client->x(), client->y(), client->width(), client->height()));
 
@@ -1498,11 +1499,11 @@ YFrameWindow *YWindowManager::manageClient(Window win, bool mapClient) {
     if (frame->affectsWorkArea())
         updateWorkArea();
     if (wmState() == wmRUNNING) {
-        if (doActivate) {
+        if (doActivate == 1) {
             frame->activateWindow(true);
             if (canManualPlace && opaqueMove)
                 frame->wmMove();
-        } else {
+        } else if (requestFocus) {
             frame->setWmUrgency(true);
         }
     }
