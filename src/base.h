79f415820bd91208dda16f280d9fe2da25412d94
Brian Bidulock <bidulock@openss7.org>
2014-11-05 03:56:28 -0700

    remove unused _icewm_deprecated declaration

--- src/base.h
+++ src/base.h
@@ -1,12 +1,6 @@
 #ifndef __BASE_H
 #define __BASE_H
 
-#if ( __GNUC__ == 3 && __GNUC_MINOR__ > 0 ) || __GNUC__ > 3
-#define _icewm_deprecated __attribute__((deprecated))
-#else
-#define _icewm_deprecated
-#endif
-
 /*** Atomar Data Types ********************************************************/
 
 #ifdef NEED_BOOL
