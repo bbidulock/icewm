bb28117e82b6f2df291bd489ec55266a6e87c600
Bert Gijsbers <gijsbers@science.uva.nl>
2017-04-29 18:59:13 +0200

    precondition simplification

--- src/debug.h
+++ src/debug.h
@@ -7,11 +7,7 @@ extern bool debug_z;
 
 #define DBG if (debug)
 #define MSG(x) DBG msg x
-#define PRECONDITION(x) \
-    if (!(x)) \
-    do { \
-    precondition("PRECONDITION FAILED at %s:%d: (" #x ")", __FILE__, __LINE__); \
-    } while (0)
+#define PRECONDITION(x) if (x); else precondition( #x , __FILE__, __LINE__)
 #else
 #define DBG if (0)
 #define MSG(x)
