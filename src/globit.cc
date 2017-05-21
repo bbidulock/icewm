61c726c40e617027a44013071c86527c82c08cda
Bert Gijsbers <gijsbers@science.uva.nl>
2017-05-21 21:12:48 +0200

    HAVE_ASPRINTF doesn't imply _GNU_SOURCE. Remove both.

--- src/globit.cc
+++ src/globit.cc
@@ -1,10 +1,5 @@
 #include "config.h"
-#ifdef HAVE_ASPRINTF
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
 #ifndef __STDC_LIMIT_MACROS
 #define __STDC_LIMIT_MACROS
 #endif
 #include "globit.c"
-#endif
