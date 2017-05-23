1acf8cbccc0f4ac0611f470f8a07d35b65b5a838
Bert Gijsbers <gijsbers@science.uva.nl>
2017-05-23 23:04:27 +0200

    A redesign with cleaner semantics and 3 subclasses for different release of dynamic memory.

--- src/ypointer.h
+++ src/ypointer.h
@@ -6,6 +6,7 @@
  *
  *  2001/08/02: Mathias Hasselmann <mathias.hasselmann@gmx.net>
  *  - initial version
+ *  2017/05/23 BG: redesign.
  */
 
 #ifndef __YPOINTER_H
@@ -13,75 +14,124 @@
 
 /*******************************************************************************
  * Smart pointers behave like pointers but delete the object they own when 
- * being destructed.
+ * being destructed. Three flavors of smart pointers are distinguished:
+ *
+ *   1. YSmartPtr destructor uses delete to deallocate the pointer data.
+ *   2. ASmartPtr destructor uses delete[] to deallocate the pointer data.
+ *   3. FSmartPtr destructor uses free to deallocate the pointer data.
+ *
+ * In addition CSmartPtr is an alias for smart new[] character strings.
+ *
+ * Please beware that currently copy constructors and copy assignment
+ * are not available. This keeps the semantics clean and simple.
+ * Open for discussion.
  ******************************************************************************/
+
+#ifndef EXIT_SUCCESS
+#include <stdlib.h>
+#endif
 
 template <class DataType>
 class YSmartPtr {
 public:
-    YSmartPtr(DataType *data = 0): 
-    fOwner(true), fData(data) {}
-
-#if 0
-    YSmartPtr(YSmartPtr &other):
-    fOwner(other.isOwner()), fData(other.release()) {}
-#endif
+    YSmartPtr(DataType *data = 0):
+        fData(data) {}
 
     virtual ~YSmartPtr() {
         unref();
     }
 
     void assign(DataType *other) {
-        if (data() != other) {
+        if (fData != other) {
             unref();
-            fOwner = true;
             fData = other;
         }
     }
 
-#if 0
     void assign(YSmartPtr &other) {
-        if (data() != other.data()) {
+        if (this != &other) {
             unref();
-            fOwner = other.isOwner();
-            fData = other.release();
+            fData = other.fData;
+            other.fData = 0;
         }
-    }
-#endif
-
-    bool equals(const YSmartPtr &other) const { 
-        return data() == other.data();
     }
 
     DataType *release() {
-        fOwner = false;
-        return data();
+        DataType *res = fData;
+        fData = 0;
+        return res;
     }
 
     DataType *data() const { return fData; }
-    bool isOwner() const { return fOwner; }
 
     void operator=(DataType *other) { assign(other); }
+    void operator=(YSmartPtr& other) { assign(other); }
     operator DataType *() const { return data(); }
-    DataType *operator*() const { return data(); }
+    DataType& operator*() const { return *data(); }
     DataType *operator->() const { return data(); }
-    bool operator==(const YSmartPtr &other) const { return equals(other); }
 
-private:
-    YSmartPtr(const DataType *data) {}
-    YSmartPtr(const YSmartPtr &other) {}
-
-    void unref() {
-        if (isOwner()) {
+protected:
+    virtual void unref() {
+        if (fData) {
             delete fData;
             fData = 0;
         }
     }
 
-    bool fOwner;
+    void reset() {
+        fData = 0;
+    }
+
+private:
     DataType *fData;
 
-    void operator=(YSmartPtr &other); // { assign(other); }
+    YSmartPtr(const YSmartPtr &other);    // no copies!
+    void operator=(const YSmartPtr &other);  // no copies!
+};
+
+template <class DataType>
+class ASmartPtr : public YSmartPtr<DataType> {
+public:
+    typedef YSmartPtr<DataType> super;
+
+    ASmartPtr(DataType *d = 0) : super(d) {}
+
+    void operator=(ASmartPtr& other) { super::assign(other); }
+
+protected:
+    virtual void unref() {
+        if (super::data()) {
+            delete[] super::data();
+            super::reset();
+        }
+    }
+};
+
+class CSmartPtr : public ASmartPtr<char> {
+public:
+    typedef ASmartPtr<char> super;
+
+    CSmartPtr(char *d = 0) : super(d) {}
+
+    void operator=(CSmartPtr& other) { super::assign(other); }
+};
+
+template <class DataType>
+class FSmartPtr : public YSmartPtr<DataType> {
+public:
+    typedef YSmartPtr<DataType> super;
+
+    FSmartPtr(DataType *d = 0) : super(d) {}
+
+    void operator=(FSmartPtr& other) { super::assign(other); }
+
+protected:
+    virtual void unref() {
+        if (super::data()) {
+            free(super::data());
+            super::reset();
+        }
+    }
 };
 
 #endif
