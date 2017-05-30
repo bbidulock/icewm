7317bd999473974f89918ba221cebd5d31328e2b
Bert Gijsbers <gijsbers@science.uva.nl>
2017-05-30 21:52:44 +0200

    Restore support for const copy operations using an ownership flag.  Improve efficiency by avoiding virtual tables. Prevent inadvertent exchange of pointers between different allocation mechanisms (free/delete/[]). Tested OK sofar.

--- src/ypointer.h
+++ src/ypointer.h
@@ -12,134 +12,162 @@
 #ifndef __YPOINTER_H
 #define __YPOINTER_H
 
-/*******************************************************************************
+/**************************************************************************
  * Smart pointers behave like pointers but delete the object they own when 
- * being destructed. Three flavors of smart pointers are distinguished:
+ * destructed. The following flavors of smart pointers are distinguished:
  *
- *   1. YSmartPtr destructor uses delete to deallocate the pointer data.
- *   2. ASmartPtr destructor uses delete[] to deallocate the pointer data.
- *   3. FSmartPtr destructor uses free to deallocate the pointer data.
+ *   1. OSmartPtr for `new` objects, deallocated by delete.
+ *   2. ASmartPtr for `new[]` arrays, deallocated by delete[].
+ *   3. FSmartPtr for malloc data, which is deallocated with free.
  *
  * In addition CSmartPtr is an alias for smart new[] character strings.
  *
- * Please beware that currently copy constructors and copy assignment
- * are not available. This keeps the semantics clean and simple.
- * Open for discussion.
- ******************************************************************************/
+ * Copy operations are allowed. Ownership of the memory is controlled by an
+ * extra parameter 'fOwn'. Only a smart pointer which owns will deallocate.
+ *
+ * The implementations are all based on a template YSmartPtr which takes
+ * as parameters the primitive type of the underlying allocated data and
+ * a type which provides the appropriate free/delete/delete[] operator.
+ *************************************************************************/
 
-#ifndef EXIT_SUCCESS
 #include <stdlib.h>
-#endif
 
-template <class DataType>
+// internal common base
+template <class DataType, class Disposer>
 class YSmartPtr {
-public:
-    YSmartPtr(DataType *data = 0):
-        fData(data) {}
+private:
+    DataType *fData;
+    bool      fOwn;
 
-    virtual ~YSmartPtr() {
-        unref();
+    void unref() {
+        if (fOwn) {
+            fOwn = false;
+            Disposer::dispose(fData);
+        }
+        fData = 0;
     }
 
-    void assign(DataType *other) {
+public:
+    explicit YSmartPtr(DataType *data = 0, bool own = true)
+        : fData(data), fOwn(own) {}
+    YSmartPtr(const YSmartPtr& other)
+        : fData(other.fData), fOwn(false) {}
+
+    ~YSmartPtr() { unref(); }
+
+    void data(DataType *other, bool own = true) {
         if (fData != other) {
             unref();
             fData = other;
         }
+        fOwn = own;
     }
 
-    void assign(YSmartPtr &other) {
+    void take(YSmartPtr& other, bool own = true) {
         if (this != &other) {
-            unref();
-            fData = other.fData;
-            other.fData = 0;
+            if (fData != other.fData) {
+                unref();
+                fData = other.fData;
+            }
+            if (other.fOwn && own) {
+                other.fOwn = false;
+                fOwn = true;
+            }
         }
     }
+
+    void copy(const YSmartPtr& other) {
+        if (this != &other) {
+            if (fData != other.fData) {
+                unref();
+                fData = other.fData;
+            }
+            fOwn = false;
+        }
+    }
+
+    void operator=(const YSmartPtr& other) { copy(other); }
 
     DataType *release() {
         DataType *res = fData;
         fData = 0;
+        fOwn = false;
         return res;
     }
 
     DataType *data() const { return fData; }
+    bool owner() const { return fOwn; }
 
-    void operator=(DataType *other) { assign(other); }
-    void operator=(YSmartPtr& other) { assign(other); }
+    bool operator==(const YSmartPtr& p) const { return fData == p.fData; }
+    bool operator!=(const YSmartPtr& p) const { return fData != p.fData; }
+    bool operator==(void *p) const { return fData == p; }
+    bool operator!=(void *p) const { return fData != p; }
+
     operator DataType *() const { return data(); }
     DataType& operator*() const { return *data(); }
     DataType *operator->() const { return data(); }
-
-protected:
-    virtual void unref() {
-        if (fData) {
-            delete fData;
-            fData = 0;
-        }
-    }
-
-    void reset() {
-        fData = 0;
-    }
-
-private:
-    DataType *fData;
-
-    YSmartPtr(const YSmartPtr &other);    // no copies!
-    void operator=(const YSmartPtr &other);  // no copies!
 };
 
+// for new objects
 template <class DataType>
-class ASmartPtr : public YSmartPtr<DataType> {
+class OSmartPtr : public YSmartPtr<DataType, OSmartPtr<DataType> > {
 public:
-    typedef YSmartPtr<DataType> super;
+    typedef YSmartPtr<DataType, OSmartPtr<DataType> > super;
 
-    ASmartPtr(DataType *d = 0) : super(d) {}
+    explicit OSmartPtr(DataType *dat = 0, bool own = true)
+        : super(dat, own) {}
+    OSmartPtr(const OSmartPtr& other)
+        : super(other) {}
 
-    virtual ~ASmartPtr() {
-        unref();
-    }
+    void operator=(const OSmartPtr& other) { super::copy(other); }
 
-    void operator=(ASmartPtr& other) { super::assign(other); }
-
-protected:
-    virtual void unref() {
-        if (super::data()) {
-            delete[] super::data();
-            super::reset();
-        }
-    }
+    static inline void dispose(DataType *ptr) { delete ptr; }
 };
 
+// for new[] arrays
+template <class DataType>
+class ASmartPtr : public YSmartPtr<DataType, ASmartPtr<DataType> > {
+public:
+    typedef YSmartPtr<DataType, ASmartPtr<DataType> > super;
+
+    explicit ASmartPtr(DataType *dat = 0, bool own = true)
+        : super(dat, own) {}
+    ASmartPtr(const ASmartPtr& other)
+        : super(other) {}
+
+    void operator=(const ASmartPtr& other) { super::copy(other); }
+
+    DataType& operator[](int index) const { return super::data()[index]; }
+
+    static inline void dispose(DataType *ptr) { delete[] ptr; }
+};
+
+// for new[] character strings
 class CSmartPtr : public ASmartPtr<char> {
 public:
     typedef ASmartPtr<char> super;
 
-    CSmartPtr(char *d = 0) : super(d) {}
+    explicit CSmartPtr(char *dat = 0, bool own = true)
+        : super(dat, own) {}
+    CSmartPtr(const CSmartPtr& other)
+        : super(other) {}
 
-    void operator=(CSmartPtr& other) { super::assign(other); }
+    void operator=(const CSmartPtr& other) { super::copy(other); }
 };
 
+// for malloc data
 template <class DataType>
-class FSmartPtr : public YSmartPtr<DataType> {
+class FSmartPtr : public YSmartPtr<DataType, FSmartPtr<DataType> > {
 public:
-    typedef YSmartPtr<DataType> super;
+    typedef YSmartPtr<DataType, FSmartPtr<DataType> > super;
 
-    FSmartPtr(DataType *d = 0) : super(d) {}
+    explicit FSmartPtr(DataType *dat = 0, bool own = true)
+        : super(dat, own) {}
+    FSmartPtr(const FSmartPtr& other) : super(other) {}
 
-    virtual ~FSmartPtr() {
-        unref();
-    }
+    void operator=(const FSmartPtr& other) { super::copy(other); }
 
-    void operator=(FSmartPtr& other) { super::assign(other); }
-
-protected:
-    virtual void unref() {
-        if (super::data()) {
-            free(super::data());
-            super::reset();
-        }
-    }
+    static inline void dispose(DataType *ptr) { free(ptr); }
 };
 
 #endif
