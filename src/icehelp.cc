649f80ef6ec73c0bb169cc5f9fc3e34493ceb891
Bert Gijsbers <gijsbers@science.uva.nl>
2017-08-13 12:54:59 +0200

    include strings.h for strcasecmp. Make History::get() return a const char*.

--- src/icehelp.cc
+++ src/icehelp.cc
@@ -10,6 +10,9 @@
 #include "yrect.h"
 #include "ascii.h"
 #include "intl.h"
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
 
 #ifdef DEBUG
 #define DUMP
@@ -747,7 +750,7 @@ public:
     History() : where(-1) { }
     bool empty() const { return array.isEmpty(); }
     int size() const { return array.getCount(); }
-    const cstring& get(int i) const { return *array[i]; }
+    const char* get(int i) const { return *array[i]; }
     void push(const mstring& s) {
         if (where == -1 || (s.nonempty() && s != get(where))) {
             for (int k = size() - 1; k > where; --k) {
