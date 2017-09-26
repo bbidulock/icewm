1f505e40bf2719f087cfe4150f4fc5af3e344876
Brian Bidulock <bidulock@openss7.org>
2017-09-25 20:06:19 -0600

    double the number of cores

--- dist.sh
+++ dist.sh
@@ -5,6 +5,5 @@
 [ -f Makefile ] && make distclean
 ./autogen.sh
 ./configure.sh
-ncores=$(nproc 2>/dev/null) || ncores=4 &&:
-make -j$ncores distcheck
+make -j$(($(nproc 2>/dev/null||echo 4)<<1)) distcheck
 
--- rebuild.sh
+++ rebuild.sh
@@ -8,10 +8,10 @@ rm -f cscope.*
 make clean
 make cscope
 cscope -b
-make -j clean all README
+make -j$(($(nproc 2>/dev/null||echo 4)<<1)) clean all README
 
 else
 # cmake cheat sheet... with Debian-style configuration
 mkdir -p builddir-debug || rm -rf builddir-debug/CMake*
-cd builddir-debug && cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DICEHELPIDX=/usr/share/doc/icewm-common/html/icewm.html -DCFGDIR=/etc/X11/icewm -DVERSION=10.9.8-debug -DDOCDIR=/usr/share/doc/icewm-common -DCMAKE_VERBOSE_MAKEFILE=ON -DCONFIG_XRANDR=on -DCONFIG_GUIEVENTS=on && make -j$(nproc || echo 3)
+cd builddir-debug && cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DICEHELPIDX=/usr/share/doc/icewm-common/html/icewm.html -DCFGDIR=/etc/X11/icewm -DVERSION=10.9.8-debug -DDOCDIR=/usr/share/doc/icewm-common -DCMAKE_VERBOSE_MAKEFILE=ON -DCONFIG_XRANDR=on -DCONFIG_GUIEVENTS=on && make -j$(($(nproc 2>/dev/null||echo 4)<<1))
 fi
