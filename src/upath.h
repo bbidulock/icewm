c2744b23309084ef9c2d1061a9fbe8bc3b984ae1
Bert Gijsbers <gijsbers@science.uva.nl>
2017-10-02 22:23:59 +0200

    XDG base dir spec: "If, when attempting to write a file, the destination directory is non-existant an attempt should be made to create it with permission 0700."

--- src/upath.h
+++ src/upath.h
@@ -38,7 +38,7 @@ public:
     bool isHttp() const;
     bool hasProtocol() const;
     int access(int mode = 0) const;
-    int mkdir(int mode = 0777) const;
+    int mkdir(int mode = 0700) const;
     int open(int flags, int mode = 0666) const;
     FILE* fopen(const char *mode) const;
     int stat(struct stat *st) const;
