0f6e75fdbe43e47495ccb1a6c70b420c1bec5661
Brian Bidulock <bidulock@openss7.org>
2017-10-15 22:24:00 -0600

    create inputOnly windows with correct parameters

--- src/wmframe.cc
+++ src/wmframe.cc
@@ -596,55 +596,54 @@ void YFrameWindow::createPointerWindows() {
     attributes.event_mask = 0;
     attrmask |= CWEventMask;
 
-    attributes.cursor = YWMApp::sizeTopPointer.handle();
     attrmask |= CWCursor;
 
-    attributes.colormap = xapp->colormap();
-    attrmask |= CWColormap;
-
-    attributes.background_pixel = xapp->black();
-    attrmask |= CWBackPixel;
-
-    attributes.border_pixel = xapp->black();
-    attrmask |= CWBorderPixel;
-
+    attributes.cursor = YWMApp::sizeTopPointer.handle();
+    fprintf(stderr, "%s: calling XCreateWindow\n", __func__);
     topSide = XCreateWindow(xapp->display(), handle(), 0, 0, 1, 1, 0,
-                            0, klass, xapp->visual(),
+                            0, klass, None,
                             attrmask, &attributes);
 
     attributes.cursor = YWMApp::sizeLeftPointer.handle();
+    fprintf(stderr, "%s: calling XCreateWindow\n", __func__);
     leftSide = XCreateWindow(xapp->display(), handle(), 0, 0, 1, 1, 0,
-                            0, klass, xapp->visual(),
+                            0, klass, None,
                             attrmask, &attributes);
 
     attributes.cursor = YWMApp::sizeRightPointer.handle();
+    fprintf(stderr, "%s: calling XCreateWindow\n", __func__);
     rightSide = XCreateWindow(xapp->display(), handle(), 0, 0, 1, 1, 0,
-                            0, klass, xapp->visual(),
+                            0, klass, None,
                             attrmask, &attributes);
 
     attributes.cursor = YWMApp::sizeBottomPointer.handle();
+    fprintf(stderr, "%s: calling XCreateWindow\n", __func__);
     bottomSide = XCreateWindow(xapp->display(), handle(), 0, 0, 1, 1, 0,
-                            0, klass, xapp->visual(),
+                            0, klass, None,
                             attrmask, &attributes);
 
     attributes.cursor = YWMApp::sizeTopLeftPointer.handle();
+    fprintf(stderr, "%s: calling XCreateWindow\n", __func__);
     topLeftCorner = XCreateWindow(xapp->display(), handle(), 0, 0, 1, 1, 0,
-                                  0, klass, xapp->visual(),
+                                  0, klass, None,
                                   attrmask, &attributes);
 
     attributes.cursor = YWMApp::sizeTopRightPointer.handle();
+    fprintf(stderr, "%s: calling XCreateWindow\n", __func__);
     topRightCorner = XCreateWindow(xapp->display(), handle(), 0, 0, 1, 1, 0,
-                                   0, klass, xapp->visual(),
+                                   0, klass, None,
                                    attrmask, &attributes);
 
     attributes.cursor = YWMApp::sizeBottomLeftPointer.handle();
+    fprintf(stderr, "%s: calling XCreateWindow\n", __func__);
     bottomLeftCorner = XCreateWindow(xapp->display(), handle(), 0, 0, 1, 1, 0,
-                                     0, klass, xapp->visual(),
+                                     0, klass, None,
                                      attrmask, &attributes);
 
     attributes.cursor = YWMApp::sizeBottomRightPointer.handle();
+    fprintf(stderr, "%s: calling XCreateWindow\n", __func__);
     bottomRightCorner = XCreateWindow(xapp->display(), handle(), 0, 0, 1, 1, 0,
-                                      0, klass, xapp->visual(),
+                                      0, klass, None,
                                       attrmask, &attributes);
 
     XMapSubwindows(xapp->display(), handle());
