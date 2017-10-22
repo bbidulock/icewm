563d73f4c0c90a886675442650daaaff6608f9af
Brian Bidulock <bidulock@openss7.org>
2017-10-22 03:49:42 -0600

    do not need this anymore

--- src/wmtaskbar.cc
+++ src/wmtaskbar.cc
@@ -164,9 +164,6 @@ TaskBar::TaskBar(IApp *app, YWindow *aParent, YActionListener *wmActionListener,
     ///setToplevel(true);
 
     initPixmaps();
-    XSetWindowBackground(xapp->display(), handle(), getTaskBarBg()->pixel());
-    if (taskbackPixmap != null)
-        XSetWindowBackgroundPixmap(xapp->display(), handle(), taskbackPixmap->pixmap());
 
     setWindowTitle(_("Task Bar"));
     setIconTitle(_("Task Bar"));
