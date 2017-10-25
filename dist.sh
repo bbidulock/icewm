0be0b5d058d6eb3ae16074bddc735e816003d73c
Bert Gijsbers <gijsbers@science.uva.nl>
2017-10-25 23:00:24 +0200

    option -j#JOBS

--- dist.sh
+++ dist.sh
@@ -2,8 +2,11 @@
 
 # a little script to create a distribution of the package
 
+[[ $1 =~ ^-j ]] && jobs=$1 ||
+    jobs=-j$(($(nproc 2>/dev/null||echo 4)<<1))
+
 [ -f Makefile ] && make distclean
 ./autogen.sh
 ./configure.sh
-make -j$(($(nproc 2>/dev/null||echo 4)<<1)) distcheck
+make "$jobs" distcheck
 
