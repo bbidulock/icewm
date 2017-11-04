118677d7f090bbf0d8a81d7bda76842c3ac4f647
Bert Gijsbers <gijsbers@science.uva.nl>
2017-11-04 08:47:22 +0100

    add options. -t runs tests on combinations of configure options.

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-if test -z "$DBGCM"; then
+unset ACONF TESTC jobs
+
+# parse argv
+for x
+do
+    case $x in
+        (-a) ACONF=1 ;;  # enable autoconf+cscope
+        (-c) DBGCM=1 ;;  # enable CMake debug build
+        (-t) TESTC=1 ;;  # test all combinations of configure options
+        (-j*) jobs=$x ;; # number of gmake procs
+        (*) echo "$0: Unknown option '$x'." >&2 ; exit 1 ;;
+    esac
+done
+
+# set default
+[[ -v ACONF || -v DBGCM || -v TESTC ]] || ACONF=1
+
+# gmake jobs
+[[ -v jobs ]] || jobs=-j$(($(nproc 2>/dev/null||echo 4)<<1))
+
+if [[ -v ACONF ]]; then
 
 rm -f cscope.*
 ./autogen.sh
@@ -8,10 +28,39 @@ rm -f cscope.*
 make clean
 make cscope
 cscope -b
-make -j$(($(nproc 2>/dev/null||echo 4)<<1)) clean all README
+make "$jobs" clean all README
 
-else
+fi
+
+if [[ -v DBGCM ]]; then
+
 # cmake cheat sheet... with Debian-style configuration
 mkdir -p builddir-debug || rm -rf builddir-debug/CMake*
-cd builddir-debug && cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DICEHELPIDX=/usr/share/doc/icewm-common/html/icewm.html -DCFGDIR=/etc/X11/icewm -DVERSION=10.9.8-debug -DDOCDIR=/usr/share/doc/icewm-common -DCMAKE_VERBOSE_MAKEFILE=ON -DCONFIG_XRANDR=on -DCONFIG_GUIEVENTS=on && make -j$(($(nproc 2>/dev/null||echo 4)<<1))
+cd builddir-debug && cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug -DICEHELPIDX=/usr/share/doc/icewm-common/html/icewm.html -DCFGDIR=/etc/X11/icewm -DVERSION=10.9.8-debug -DDOCDIR=/usr/share/doc/icewm-common -DCMAKE_VERBOSE_MAKEFILE=ON -DCONFIG_XRANDR=on -DCONFIG_GUIEVENTS=on && make "$jobs"
+
 fi
+
+# test combinations of configure options
+if [[ -v TESTC ]]; then
+    rm -f -- rebuild.log rebuild.err
+    for i in {001..050} :; do
+        rm -f -- rebuild.tmp
+        make "$jobs" clean &>>rebuild.log
+        # pick ten configure options randomly:
+        able=$(./configure --help=short |
+              sed -e 's|\[.*||' |
+              awk '{print $1}' |
+              grep -e --enable- -e --disable- |
+              grep -v -e -FEATURE -e -checking -e -tracking -e -maintainer |
+              grep -v -e -rules -e -install -e -libtool -e -static -e -shared |
+              shuf -n 10)
+        echo "# $i: $(date +%T): ./configure $able" | tee rebuild.tmp
+        ./configure $able --disable-xrender &>>rebuild.tmp &&
+        make "$jobs" &>>rebuild.tmp ||
+        { echo "FAILED for $able !" |
+            tee -a rebuild.err;
+            mv -fv rebuild.tmp rebuild-$i.err
+        }
+    done |& tee -a rebuild.log
+fi
+
