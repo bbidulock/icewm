9a3513ad7b41d1e3d4d8e2c2e9c45469cb9334e9
Bert Gijsbers <gijsbers@science.uva.nl>
2017-11-21 22:45:40 +0100

    /bin/echo doesn't work on OpenBSD, use printf instead.

--- autogen.sh
+++ autogen.sh
@@ -28,7 +28,7 @@ if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 	       s:%%BRANCH%%:$BRANCH:g"
 	sed -r -e "$subst" icewm.spec.in >icewm.spec
 	sed -r -e "$subst" icewm.lsm.in >icewm.lsm
-	/bin/echo -e "PACKAGE=$PACKAGE\nVERSION=$VERSION" >VERSION
+	printf "PACKAGE=%s\nVERSION=%s\n" "$PACKAGE" "$VERSION" >VERSION
 else
 	sed -i.bak configure.ac -r \
 		-e "s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
