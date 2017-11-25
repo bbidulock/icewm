604315ffbf6315da23fb9516393d8fdf91bfb23d
Bert Gijsbers <gijsbers@science.uva.nl>
2017-11-25 03:46:02 +0100

    Substructure events didn't work for hexchat. Add YXEmbedClient::handleConfigure to catch hexchat resizing its icon. Add a small delay when registering a new tray app to avoid race conditions. Avoid obnoxious error messages for already destroyed tray apps.

--- src/yxembed.cc
+++ src/yxembed.cc
@@ -11,27 +11,28 @@ YXEmbed::~YXEmbed() {
 
 YXEmbedClient::YXEmbedClient(YXEmbed *embedder, YWindow *aParent, Window win):
     YWindow(aParent, win),
+    fEmbedder(embedder),
     _XEMBED_INFO("_XEMBED_INFO")
 {
-    fEmbedder = embedder;
     setParentRelative();
 }
 
 YXEmbedClient::~YXEmbedClient() {
 }
 
-void YXEmbedClient::handleDestroyWindow(const XDestroyWindowEvent &destroyWindow) {
-    MSG(("embed client destroy"));
+void YXEmbedClient::handleDestroyWindow(const XDestroyWindowEvent& destroy) {
+    MSG(("embed client destroy: evt 0x%lX, win 0x%lX",
+                destroy.event, destroy.window));
 
-    if (destroyWindow.window == handle()) {
-        if (false == fEmbedder->destroyedClient(handle())) {
-            YWindow::handleDestroyWindow(destroyWindow);
-        }
+    if (destroy.window == handle()) {
+        YWindow::handleDestroyWindow(destroy);
+        fEmbedder->destroyedClient(handle());
     }
 }
 
 void YXEmbedClient::handleReparentNotify(const XReparentEvent &reparent) {
-    MSG(("embed client reparent"));
+    MSG(("embed client reparent: win 0x%lX, par 0x%lX",
+                reparent.window, reparent.parent));
 
     if (reparent.window == handle() &&
             reparent.parent != fEmbedder->handle()) {
@@ -41,8 +42,26 @@ void YXEmbedClient::handleReparentNotify(const XReparentEvent &reparent) {
     }
 }
 
+void YXEmbedClient::handleConfigure(const XConfigureEvent& event) {
+    MSG(("embed client configure: evt 0x%lX, win 0x%lX, width %d, height %d",
+          event.event, event.window, event.width, event.height));
+
+    YWindow::handleConfigure(event);
+
+    int ww = (int) fEmbedder->width();
+    int hh = (int) fEmbedder->height();
+    if (ww >= 16 && hh >= 16 &&
+        (!inrange(event.width,  ww - 4, ww + 8) ||
+         !inrange(event.height, hh - 4, hh + 1)))
+    {
+        MSG(("embed client configure: setSize ww %u, hh %u", ww, hh));
+        setSize(ww, hh);
+    }
+}
+
 void YXEmbedClient::handleProperty(const XPropertyEvent &property) {
-    MSG(("embed client property"));
+    MSG(("embed client property: win 0x%lX, atom %ld",
+                property.window, property.atom));
 
     if ((property.window == handle()) &&
             property.atom == _XEMBED_INFO) {
@@ -67,8 +86,9 @@ void YXEmbedClient::handleProperty(const XPropertyEvent &property) {
     }
 }
 
-void YXEmbedClient::handleUnmap(const XUnmapEvent &/*unmap*/) {
-//    YWindow::handleUnmap(unmap);
+void YXEmbedClient::handleUnmap(const XUnmapEvent& unmap) {
+    MSG(("embed client unmap: evt 0x%lX, win 0x%lX", unmap.event, unmap.window));
+
     fEmbedder->handleClientUnmap(handle());
 }
 
