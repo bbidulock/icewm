ac8c2f4a79faf860fe4bb4982414c9b44f2d3a21
Bert Gijsbers <gijsbers@science.uva.nl>
2018-02-25 21:07:26 +0100

    port to FreeBSD

--- src/ylocale.h
+++ src/ylocale.h
@@ -15,7 +15,7 @@
 #error libiconv in use but included iconv.h not from libiconv
 #endif
 #if !defined(CONFIG_LIBICONV) && defined (_LIBICONV_VERSION)
-#if !defined(__OpenBSD__)
+#if !defined(__OpenBSD__) && !defined(__FreeBSD__)
 #error libiconv not in use but included iconv.h is from libiconv
 #endif
 #endif
