a549f005166f2afcde4ca841a07b70bd643bafaa
Brian Bidulock <bidulock@openss7.org>
2018-06-05 14:45:00 -0600

    Revert "keep --iconformats undocumented for now."
    This reverts commit 1ef85b0485d304814bf4180ff64fae2861da1124.

--- src/wmapp.cc
+++ src/wmapp.cc
@@ -1355,6 +1355,7 @@ static void print_usage(const char *argv0) {
              "\n"
              "  --configured        Print the compile time configuration.\n"
              "  --directories       Print the configuration directories.\n"
+             "  --iconformats       Print the supported icon image formats.\n"
              "  -l, --list-themes   Print a list of all available themes.\n"
              "\n"
              "Environment variables:\n"
