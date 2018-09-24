69026c171717125c0efe65084a42f39eede4ebc4
Eduard Bloch <blade@debian.org>
2018-09-24 17:57:51 +0200

    Revert "Utilize auto_raii helper to avoid memleak on globit responses"
    This reverts commit eb8af4ae037dd76248097804a3db6621cb771aac.

--- src/yinput.cc
+++ src/yinput.cc
@@ -678,12 +678,10 @@ void YInputLine::autoScroll(int delta, const XMotionEvent *motion) {
 }
 
 void YInputLine::complete() {
-    char *res=NULL;
-    cstring t(fText);
-    int res_count = globit_best(t.c_str(), &res);
-    auto_raii<void*,free> cleanup(res);
-    // only exact match
-    if(1 == res_count) setText(ustring(res, strlen(res)));
+    char* res = 0;
+    if (1 == globit_best(cstring(fText), &res))
+        setText(res);
+    free(res);
 }
 
 // vim: set sw=4 ts=4 et:
