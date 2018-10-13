15012e018e6580de9952f6dbad037bd1063c6b42
Bert Gijsbers <gijsbers@science.uva.nl>
2018-10-13 08:10:03 +0200

    fix WM_CLASS for NetStatus for #232.

--- src/default.h
+++ src/default.h
@@ -202,7 +202,7 @@ XSV(const char *, cpuClassHint,                 "top.XTerm")
 XIV(bool, cpuCombine,                           true)
 
 #ifdef __linux__
-XSV(const char *, netCommand,                   "xterm -name 'socket statistics' -title 'Socket Statistics' -hold -e ss")
+XSV(const char *, netCommand,                   "xterm -name 'ss' -title 'Socket Statistics' -hold -e ss")
 XSV(const char *, netClassHint,                 "ss.XTerm")
 #else
 XSV(const char *, netCommand,                   "xterm -name netstat -title 'Network Status' -hold -e netstat -c")
