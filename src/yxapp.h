bbidulock/icewm

commit 4d2ddd85d31bbe7fac82b6a9376cfa7052f48841

YTextProperty

src/yxapp.h
@@ -16,6 +16,12 @@ public:
     const char* str() const { return name; }
 };
 
+class YTextProperty : public XTextProperty {
+public:
+    YTextProperty(const char* str);
+    ~YTextProperty();
+};
+
 class YXPoll: public YPoll<class YXApplication> {
 public:
     virtual void notifyRead();

src/yxapp.cc
@@ -1289,4 +1289,16 @@ YAtom::operator Atom() {
     return atom;
 }
 
+YTextProperty::YTextProperty(const char* str) {
+    encoding = XA_STRING;
+    format = 8;
+    nitems = strlen(str);
+    value = new unsigned char[1 + nitems];
+    if (value) memcpy(value, str, 1 + nitems);
+}
+
+YTextProperty::~YTextProperty() {
+    if (value) delete[] value;
+}
+
 // vim: set sw=4 ts=4 et:

commit a29d2c8566da4b9cc694ac9cf857ede21d80f3da

Build fix (XTextProperty definition)
Was broken since 4d2ddd85d31bbe7fac82b6a9376cfa7052f48841 at least for
libx11-dev 2:1.6.7-1

src/yxapp.h
@@ -3,6 +3,7 @@
 
 #include "yapp.h"
 #include "ywindow.h"
+#include <X11/Xutil.h>
 
 class YAtom {
     const char* name;