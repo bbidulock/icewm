1251fb71f50288019f58722ea98cc566e3751fd4
Brian Bidulock <bidulock@openss7.org>
2020-02-08 03:15:30 -0700

    do not pad day number in MDOCDATE

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]
 if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
 	VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
 	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
-	MDOCDATE=$(date --date="$DATE" +'%B %d, %Y')
+	MDOCDATE=$(date --date="$DATE" +'%B %-d, %Y')
 	BRANCH=$(git tag --sort=-creatordate|head -1)
 	GNITS="gnits "
 	if [ "$VERSION" != "$BRANCH" ]; then
