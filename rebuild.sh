a13793d2bd4e6e3fdf1306b97807ad019875c066
Bert Gijsbers <gijsbers@science.uva.nl>
2020-06-03 05:43:24 +0200

    Add a cmake release and --prefix.

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-unset ACONF TESTC jobs
+unset ACONF DBGCM RELCM TESTC jobs prefix xterm
+prefix=/usr
+xterm=urxvt
 
 # parse argv
 for x
@@ -8,14 +10,17 @@ do
     case $x in
         (-a) ACONF=1 ;;  # enable autoconf+cscope
         (-c) DBGCM=1 ;;  # enable CMake debug build
+        (-r) RELCM=1 ;;  # enable CMake release build
         (-t) TESTC=1 ;;  # test all combinations of configure options
         (-j*) jobs=$x ;; # number of gmake procs
+        (--prefix=*) prefix=${x#*=} ;; # install prefix
+        (--with-xterm=*) xterm=${x#*=} ;; # set terminal
         (*) echo "$0: Unknown option '$x'." >&2 ; exit 1 ;;
     esac
 done
 
 # set default
-[[ -v ACONF || -v DBGCM || -v TESTC ]] || ACONF=1
+[[ -v ACONF || -v DBGCM || -v RELCM || -v TESTC ]] || ACONF=1
 
 # gmake jobs
 [[ -v jobs ]] || jobs=-j$(($(nproc 2>/dev/null||echo 4)<<1))
@@ -53,21 +58,41 @@ if [[ -v DBGCM ]]; then
     # cmake cheat sheet... with Debian-style configuration
     mkdir -p builddir-debug || rm -rf builddir-debug/CMake*
     cd builddir-debug &&
-        cmake .. \
+    cmake .. \
         -DCONFIG_GDK_PIXBUF_XLIB=ON \
         -DCONFIG_LIBRSVG=ON \
+        -DCONFIG_XRANDR=ON \
         -DCONFIG_XPM=ON \
         -DCMAKE_CXX_COMPILER_ID=clang \
         -DCMAKE_CXX_COMPILER=clang++ \
-        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DCMAKE_INSTALL_PREFIX=$prefix \
         -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-        -DICEHELPIDX=/usr/share/doc/icewm-common/html/icewm.html \
+        -DICEHELPIDX=$prefix/share/doc/icewm-common/html/icewm.html \
         -DCFGDIR=/etc/X11/icewm \
         -DVERSION=10.9.8-debug \
-        -DDOCDIR=/usr/share/doc/icewm-common \
+        -DDOCDIR=$prefix/share/doc/icewm-common \
         -DCMAKE_VERBOSE_MAKEFILE=ON \
-        -DCONFIG_XRANDR=on &&
-        make "$jobs"
+        -DXTERMCMD=$xterm &&
+    make "$jobs"
+fi
+
+if [[ -v RELCM ]]; then
+
+    # cmake release configuration
+    [[ -d build ]] && rm -rf build
+    mkdir -m 0700 build &&
+    cd build &&
+    cmake .. \
+        -DCMAKE_BUILD_TYPE=MinSizeRel \
+        -DCMAKE_INSTALL_PREFIX=$prefix \
+        -DCONFIG_GDK_PIXBUF_XLIB=ON \
+        -DCONFIG_LIBRSVG=ON \
+        -DCONFIG_XPM=ON \
+        -DCMAKE_VERBOSE_MAKEFILE=ON \
+        -DCONFIG_XRANDR=ON \
+        -DENABLE_LTO=ON \
+        -DXTERMCMD=$xterm &&
+    make "$jobs"
 fi
 
 # test combinations of configure options
@@ -76,7 +101,7 @@ if [[ -v TESTC ]]; then
     for i in {001..050} :; do
         rm -f -- rebuild.tmp
         make "$jobs" clean &>>rebuild.log
-        # pick ten configure options randomly:
+        # pick five configure options randomly:
         able=$(./configure --help=short |
               sed -e 's|\[.*||' |
               awk '{print $1}' |
@@ -85,7 +110,7 @@ if [[ -v TESTC ]]; then
               grep -v -e -rules -e -install -e -libtool -e -static -e -shared |
               shuf -n 5)
         echo "# $i: $(date +%T): ./configure $able" | tee rebuild.tmp
-        ./configure $able --disable-xrender &>>rebuild.tmp &&
+        ./configure $able &>>rebuild.tmp &&
         make "$jobs" &>>rebuild.tmp ||
         { echo "FAILED for $able !" |
             tee -a rebuild.err;
