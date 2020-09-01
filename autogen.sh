037725535ec5a9533737f5a4845f7f96cc6844cd
Eduard Bloch <blade@debian.org>
2020-09-01 20:48:51 +0200

    Cope with shallow clones

--- autogen.sh
+++ autogen.sh
@@ -11,7 +11,8 @@ PACKAGE=$(grep AC_INIT configure.ac|head -1|sed -r 's,AC_INIT[(][[],,;s,[]].*,,'
 GTVERSION=$(gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*)\.[^\.]*$,\1,;s,(^[^\.]*\.[^\.]*\.[^\.]*)\.[^\.]*$,\1,')
 
 if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
-	VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
+  VERSION_RAW=$(git describe --tags || echo unknown-dummy-version)
+	VERSION=$(echo $VERSION_RAW | sed 's,[-_],.,g;s,\.g.*$,,')
 	DATE=$(git show -s --format=%ci HEAD^{commit}|awk '{print$1}')
 	MDOCDATE=$(date --date="$DATE" +'%B %-d, %Y')
 	BRANCH=$(git tag --sort=-creatordate|head -1)
