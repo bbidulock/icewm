8e9661c6334decbf533af1a38ef97b4203f38bfe
Bert Gijsbers <gijsbers@science.uva.nl>
2021-03-18 21:46:16 +0100

    Move ObjectButton to its own source file. Rename yinput.cc to yinputline.cc.

--- src/objbar.cc
+++ src/objbar.cc
@@ -7,17 +7,8 @@
 #include "objmenu.h"
 #include "objbar.h"
 #include "objbutton.h"
-#include "ybutton.h"
-#include "prefs.h"
 #include "wmtaskbar.h"
-#include "wmapp.h"
 #include "wpixmaps.h"
-#include "yrect.h"
-#include "yicon.h"
-
-ref<YFont> ObjectButton::font;
-YColorName ObjectButton::bgColor(&clrToolButton);
-YColorName ObjectButton::fgColor(&clrToolButtonText);
 
 ObjectBar::ObjectBar(YWindow *parent): YWindow(parent) {
     setParentRelative();
@@ -97,78 +88,8 @@ void ObjectBar::handleExpose(const XExposeEvent& e) {
     }
 }
 
-ref<YFont> ObjectButton::getFont() {
-    return font != null ? font : font =
-        (*toolButtonFontName ? YFont::getFont(XFA(toolButtonFontName))
-         : YButton::getFont());
-}
-
-YColor ObjectButton::getColor() {
-    return fgColor ? fgColor : YButton::getColor();
-}
-
-YSurface ObjectButton::getSurface() {
-    return YSurface(bgColor ? bgColor : YButton::normalButtonBg,
-                    toolbuttonPixmap, toolbuttonPixbuf);
-}
-
-void ObjectButton::actionPerformed(YAction action, unsigned modifiers) {
-    wmapp->signalGuiEvent(geLaunchApp);
-    if (fObject) fObject->open();
-    else YButton::actionPerformed(action, modifiers);
-}
-
-void ObjectButton::configure(const YRect2& r) {
-    if (r.resized()) {
-        repaint();
-    }
-}
-
-void ObjectButton::paint(Graphics &g, const YRect &r) {
-    if (taskbackPixbuf == null && taskbackPixmap != null) {
-        g.fillPixmap(taskbackPixmap, r.x(), r.y(),
-                     r.width(), r.height(), r.x() + x(), r.y() + y());
-    }
-    YButton::paint(g, r);
-}
-
-void ObjectButton::repaint() {
-    GraphicsBuffer(this).paint();
-}
-
-void ObjectButton::requestFocus(bool requestUserFocus) {
-    if (fMenu && hasPopup() == false) {
-        setPopup(fMenu->ymenu());
-    }
-    YButton::requestFocus(requestUserFocus);
-}
-
-void ObjectButton::popupMenu() {
-    if (fMenu && hasPopup() == false) {
-        setPopup(fMenu->ymenu());
-    }
-    YButton::popupMenu();
-}
-
 void ObjectBar::handleButton(const XButtonEvent& up) {
     parent()->handleButton(up);
-}
-
-void ObjectButton::handleButton(const XButtonEvent& up) {
-    if (up.button == Button1) {
-        YButton::handleButton(up);
-    } else {
-        parent()->handleButton(up);
-    }
-}
-
-void ObjectButton::updateToolTip() {
-    if (getToolTip() == null) {
-        xsmart<char> name;
-        if (fetchTitle(&name)) {
-            setToolTip(mstring(name));
-        }
-    }
 }
 
 void ObjectBar::refresh() {
---
+++ src/objbutton.cc
@@ -0,0 +1,88 @@
+/*
+ * IceWM
+ *
+ * Copyright (C) 1999-2002 Marko Macek
+ */
+#include "config.h"
+#include "objbutton.h"
+#include "prefs.h"
+#include "wmapp.h"
+#include "wpixmaps.h"
+#include "yicon.h"
+#include "obj.h"
+
+ref<YFont> ObjectButton::font;
+YColorName ObjectButton::bgColor(&clrToolButton);
+YColorName ObjectButton::fgColor(&clrToolButtonText);
+
+ref<YFont> ObjectButton::getFont() {
+    return font != null ? font : font =
+        (*toolButtonFontName ? YFont::getFont(XFA(toolButtonFontName))
+         : YButton::getFont());
+}
+
+YColor ObjectButton::getColor() {
+    return fgColor ? fgColor : YButton::getColor();
+}
+
+YSurface ObjectButton::getSurface() {
+    return YSurface(bgColor ? bgColor : YButton::normalButtonBg,
+                    toolbuttonPixmap, toolbuttonPixbuf);
+}
+
+void ObjectButton::actionPerformed(YAction action, unsigned modifiers) {
+    wmapp->signalGuiEvent(geLaunchApp);
+    if (fObject) fObject->open();
+    else YButton::actionPerformed(action, modifiers);
+}
+
+void ObjectButton::configure(const YRect2& r) {
+    if (r.resized()) {
+        repaint();
+    }
+}
+
+void ObjectButton::paint(Graphics &g, const YRect &r) {
+    if (taskbackPixbuf == null && taskbackPixmap != null) {
+        g.fillPixmap(taskbackPixmap, r.x(), r.y(),
+                     r.width(), r.height(), r.x() + x(), r.y() + y());
+    }
+    YButton::paint(g, r);
+}
+
+void ObjectButton::repaint() {
+    GraphicsBuffer(this).paint();
+}
+
+void ObjectButton::requestFocus(bool requestUserFocus) {
+    if (fMenu && hasPopup() == false) {
+        setPopup(fMenu->ymenu());
+    }
+    YButton::requestFocus(requestUserFocus);
+}
+
+void ObjectButton::popupMenu() {
+    if (fMenu && hasPopup() == false) {
+        setPopup(fMenu->ymenu());
+    }
+    YButton::popupMenu();
+}
+
+void ObjectButton::handleButton(const XButtonEvent& up) {
+    if (up.button == Button1) {
+        YButton::handleButton(up);
+    } else {
+        parent()->handleButton(up);
+    }
+}
+
+void ObjectButton::updateToolTip() {
+    if (getToolTip() == null) {
+        xsmart<char> name;
+        if (fetchTitle(&name)) {
+            setToolTip(mstring(name));
+        }
+    }
+}
+
+// vim: set sw=4 ts=4 et:
