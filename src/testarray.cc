8ad5ae1c2a9c6d456fabaa0b0b85b3cd5a8a0531
Bert Gijsbers <gijsbers@science.uva.nl>
2021-04-09 21:26:55 +0200

    Make testarray.cc mostly silent by default.

--- src/testarray.cc
+++ src/testarray.cc
@@ -5,11 +5,35 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <assert.h>
 #include <sys/time.h>
+
+#define assert(a) if ((a) != 0) okays++; else bad(#a, __LINE__)
 
 char const *ApplicationName("testarray");
 bool multiByte(true);
+static bool test_time(false);
+static bool test_dump(false);
+static int fails;
+static int okays;
+static int total;
+
+static void bad(const char* str, int line) {
+    fails++;
+    printf("%s: test failed at line %d\n", ApplicationName, line);
+}
+
+static void report(const char* mod) {
+    int done = fails + okays;
+    if (fails) {
+        printf("%s: %6s: %d/%d tests failed, %d/%d tests succeeded\n",
+                ApplicationName, mod+5, fails, done, okays, done);
+    } else {
+        printf("%s: %6s: %d/%d tests succeeded\n",
+                ApplicationName, mod+5, okays, done);
+    }
+    total += fails;
+    fails = okays = 0;
+}
 
 class watch {
     double start;
@@ -33,37 +57,43 @@ int strnullcmp(const char *a, const char *b) {
 }
 
 static void dump(const char *label, const YArray<int> &array) {
-    printf("%s: count=%d, capacity=%d\n  content={",
-           label, array.getCount(), array.getCapacity());
+    if (test_dump) {
+        printf("%s: count=%d, capacity=%d\n  content={",
+               label, array.getCount(), array.getCapacity());
 
-    if (array.nonempty()) {
-        printf(" %d", array[0]);
+        if (array.nonempty()) {
+            printf(" %d", array[0]);
 
-        for (YArray<int>::SizeType i = 1; i < array.getCount(); ++i)
-            printf(", %d", array[i]);
+            for (YArray<int>::SizeType i = 1; i < array.getCount(); ++i)
+                printf(", %d", array[i]);
+        }
+
+        puts(" }");
     }
-
-    puts(" }");
 }
 
 static void dump(const char *label, const YArray<const char *> &array) {
-    printf("%s: count=%d, capacity=%d\n  content={",
-           label, array.getCount(), array.getCapacity());
+    if (test_dump) {
+        printf("%s: count=%d, capacity=%d\n  content={",
+               label, array.getCount(), array.getCapacity());
 
-    for (YArray<const char *>::SizeType i = 0; i < array.getCount(); ++i)
-        printf(" %s", array[i]);
+        for (YArray<const char *>::SizeType i = 0; i < array.getCount(); ++i)
+            printf(" %s", array[i]);
 
-    puts(" }");
+        puts(" }");
+    }
 }
 
 static void dump(const char *label, const YStringArray &array) {
-    printf("%s: count=%d, capacity=%d\n  content={",
-           label, array.getCount(), array.getCapacity());
+    if (test_dump) {
+        printf("%s: count=%d, capacity=%d\n  content={",
+               label, array.getCount(), array.getCapacity());
 
-    for (YStringArray::SizeType i = 0; i < array.getCount(); ++i)
-        printf(" %s", array[i]);
+        for (YStringArray::SizeType i = 0; i < array.getCount(); ++i)
+            printf(" %s", array[i]);
 
-    puts(" }");
+        puts(" }");
+    }
 }
 
 static const char *cmp(const YStringArray &a, const YStringArray &b) {
@@ -96,10 +126,9 @@ static const char *cmp(const YStringArray &a, const YStringArray &b) {
 static void test_int() {
     YArray<int> a;
 
-    puts("testing append YArray<int>");
-
     for (int i = 0; i < 13; ++i) {
         dump("Array<int>", a); a.append(i);
+        assert(a.getCount() == i+1 && a[i] == i);
     }
     assert(a.getCount() == 13);
     assert(a[1] = 1);
@@ -107,27 +136,28 @@ static void test_int() {
 
     dump("Array<int>", a);
 
-    puts("");
-    puts("testing insert for YArray<int>");
-
     a.insert(5, -1); dump("Array<int>", a);
+    assert(a.getCount() == 14 && a[5] == -1);
     a.insert(13, -2); dump("Array<int>", a);
+    assert(a.getCount() == 15 && a[13] == -2);
     a.insert(15, -3); dump("Array<int>", a);
+    assert(a.getCount() == 16 && a[15] == -3);
     a.insert(16, -4); dump("Array<int>", a);
+    assert(a.getCount() == 17 && a[16] == -4);
     //    a.insert(42, -5); dump("Array<int>", a);
     //    a.insert(160, -6); dump("Array<int>", a);
     a.insert(0, -7); dump("Array<int>", a);
-    assert(a.getCount() == 18);
+    assert(a.getCount() == 18 && a[0] == -7);
     assert(a[6] == -1);
     assert(a[17] == -4);
+    a.remove(13);
+    a.remove(13);
+    a.remove(13);
+    assert(a[13] == -3);
 
-    puts("");
-    puts("testing clear for YArray<int>");
     a.clear(); dump("Array<int>", a);
     assert(a.isEmpty());
 
-    puts("");
-    puts("another insertion test for YArray<int>");
     a.insert(0, 1); dump("Array<int>: inserted 1@0", a);
     a.insert(1, 2); dump("Array<int>: inserted 2@1", a);
     a.insert(1, 3); dump("Array<int>: inserted 3@1", a);
@@ -137,65 +167,87 @@ static void test_int() {
     assert(a.getCount() == 6);
     assert(a[5] == 2);
 
-    puts("");
+    report(__func__);
 }
 
 static void test_cptr() {
-    puts("testing append for YArray<const char *>");
-
     YArray<const char *> b;
+    assert(b.getCount() == 0);
     dump("Array<const char *>", b); b.append("this");
+    assert(b.getCount() == 1 && !strcmp(b[0], "this"));
     dump("Array<const char *>", b); b.append("is");
+    assert(b.getCount() == 2 && !strcmp(b[0], "this"));
+    assert(b.getCount() == 2 && !strcmp(b[1], "is"));
     dump("Array<const char *>", b); b.append("the");
+    assert(b.getCount() == 3 && !strcmp(b[0], "this"));
+    assert(b.getCount() == 3 && !strcmp(b[1], "is"));
+    assert(b.getCount() == 3 && !strcmp(b[2], "the"));
     dump("Array<const char *>", b); b.append("stinking");
+    assert(b.getCount() == 4 && !strcmp(b[0], "this"));
+    assert(b.getCount() == 4 && !strcmp(b[1], "is"));
+    assert(b.getCount() == 4 && !strcmp(b[2], "the"));
+    assert(b.getCount() == 4 && !strcmp(b[3], "stinking"));
     dump("Array<const char *>", b); b.append("foo");
+    assert(b.getCount() == 5 && !strcmp(b[0], "this"));
+    assert(b.getCount() == 5 && !strcmp(b[1], "is"));
+    assert(b.getCount() == 5 && !strcmp(b[2], "the"));
+    assert(b.getCount() == 5 && !strcmp(b[3], "stinking"));
+    assert(b.getCount() == 5 && !strcmp(b[4], "foo"));
     dump("Array<const char *>", b); b.append("bar");
+    assert(b.getCount() == 6 && !strcmp(b[0], "this"));
+    assert(b.getCount() == 6 && !strcmp(b[1], "is"));
+    assert(b.getCount() == 6 && !strcmp(b[2], "the"));
+    assert(b.getCount() == 6 && !strcmp(b[3], "stinking"));
+    assert(b.getCount() == 6 && !strcmp(b[4], "foo"));
+    assert(b.getCount() == 6 && !strcmp(b[5], "bar"));
     dump("Array<const char *>", b);
 
-    puts("");
+    report(__func__);
 }
 
 static void test_str() {
-    puts("testing append for YStringArray");
-
     YStringArray c;
     dump("YStringArray", c); c.append("this");
+    assert(c.getCount() == 1 && !strcmp(c[0], "this"));
     dump("YStringArray", c); c.append("is");
+    assert(c.getCount() == 2 && !strcmp(c[1], "is"));
     dump("YStringArray", c); c.append("the");
+    assert(c.getCount() == 3 && !strcmp(c[2], "the"));
     dump("YStringArray", c); c.append("stinking");
+    assert(c.getCount() == 4 && !strcmp(c[3], "stinking"));
     dump("YStringArray", c); c.append("foo");
+    assert(c.getCount() == 5 && !strcmp(c[4], "foo"));
     dump("YStringArray", c); c.append("bar");
+    assert(c.getCount() == 6 && !strcmp(c[5], "bar"));
     dump("YStringArray", c);
-
-    puts("");
-
-    puts("copy constructors for YStringArray");
 
     YStringArray orig;
     orig.append("check");
     orig.append("this");
     orig.append("out");
     dump("orig", orig);
+    assert(orig.getCount() == 3);
 
     const YStringArray copy(orig);
+    assert(orig.getCount() == 0);
+    assert(copy.getCount() == 3);
     const YStringArray copy2(copy);
+    assert(orig.getCount() == 0);
+    assert(copy.getCount() == 3);
+    assert(copy2.getCount() == 3);
 
     dump("orig", orig);
     dump("copy", copy);
     dump("copy2", copy2);
 
-    printf("orig vs. copy: %s\n", cmp(orig, copy));
-    printf("orig vs. copy2: %s\n", cmp(orig, copy2));
-    printf("copy vs. copy2: %s\n", cmp(copy, copy2));
+    if (orig.getCount() || copy.getCount() != 3)
+        printf("orig vs. copy: %s\n", cmp(orig, copy));
+    if (orig.getCount() || copy2.getCount() != 3)
+        printf("orig vs. copy2: %s\n", cmp(orig, copy2));
+    if (copy.getCount() != 3 || copy2.getCount() != 3)
+        printf("copy vs. copy2: %s\n", cmp(copy, copy2));
 
-    for (int i = 0; i < copy.getCount(); ++i) {
-        printf("C copy[%d] = %s.\n", i, copy.getCArray()[i]);
-    }
-    for (int i = 0; i < copy2.getCount(); ++i) {
-        printf("C copy2[%d] = %s.\n", i, copy2.getCArray()[i]);
-    }
-
-    puts("");
+    report(__func__);
 }
 
 static void test_mstr() {
@@ -245,7 +297,9 @@ static void test_mstr() {
         assert(ma[i] == ms[k]);
     }
 
-    printf("tested MStringArray OK (%s)\n\n", mark.report());
+    if (test_time)
+        printf("tested MStringArray OK (%s)\n\n", mark.report());
+    report(__func__);
 }
 
 /*
@@ -317,17 +371,35 @@ static void test_refstr() {
         assert(!strcmp(*ma[i], ms[k]));
     }
 
-    printf("tested refstring array OK (%s)\n\n", mark.report());
+    if (test_time)
+        printf("tested refstring array OK (%s)\n\n", mark.report());
+    report(__func__);
 }
 
-int main() {
+static void test_options(int argc, char** argv) {
+    for (int i = 1; i < argc; ++i) {
+        char* s = argv[i];
+        if (!strcmp(s, "-t") || !strcmp(s, "--time")) {
+            test_time = true;
+        }
+        else if (!strcmp(s, "-d") || !strcmp(s, "--dump")) {
+            test_dump = true;
+        }
+        else {
+            assert(!"invalid option");
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    test_options(argc, argv);
     test_int();
     test_cptr();
     test_str();
     test_mstr();
     test_refstr();
 
-    return 0;
+    return total != 0;
 }
 
 // vim: set sw=4 ts=4 et:
