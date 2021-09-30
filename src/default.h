5282953d11afd7dc454bd806905ffecb5841c1c6
Bert Gijsbers <gijsbers@science.uva.nl>
2021-09-30 22:28:07 +0200

    Clarify some preferences.

--- src/default.h
+++ src/default.h
@@ -542,16 +542,16 @@ cfoption icewm_preferences[] = {
     OKV("KeySysTileHorizontal",                 gKeySysTileHorizontal,          "Tiles all windows from top to bottom maximized horizontally."),
     OKV("KeySysCascade",                        gKeySysCascade,                 "Makes a horizontal cascade of all windows which are maximized vertically."),
     OKV("KeySysArrange",                        gKeySysArrange,                 "Rearranges the windows."),
-    OKV("KeySysArrangeIcons",                   gKeySysArrangeIcons,            "Rearranges icons."),
+    OKV("KeySysArrangeIcons",                   gKeySysArrangeIcons,            "Rearranges the icons on the desktop, if MinimizeToDesktop is true."),
     OKV("KeySysMinimizeAll",                    gKeySysMinimizeAll,             "Minimizes all windows."),
     OKV("KeySysHideAll",                        gKeySysHideAll,                 "Hides all windows."),
     OKV("KeySysUndoArrange",                    gKeySysUndoArrange,             "Undoes arrangement."),
-    OKV("KeySysShowDesktop",                    gKeySysShowDesktop,             "Unmaps all windows to show the desktop."),
-    OKV("KeySysCollapseTaskBar",                gKeySysCollapseTaskBar,         "Hides the task bar."),
+    OKV("KeySysShowDesktop",                    gKeySysShowDesktop,             "Minimizes all windows to show the desktop."),
+    OKV("KeySysCollapseTaskBar",                gKeySysCollapseTaskBar,         "Toggles displaying the task bar."),
 
-    OKF("WorkspaceNames", addWorkspace, "Add a workspace"),
-    OKF("KeyboardLayouts", addKeyboard, "Add a keyboard layout"),
-    OSV("WinMenuItems",                         &winMenuItems,                  "Items supported in menu window (rmsnxfhualytickw)"),
+    OKF("WorkspaceNames",                       addWorkspace, "Add a workspace"),
+    OKF("KeyboardLayouts",                      addKeyboard, "Add a keyboard layout"),
+    OSV("WinMenuItems",                         &winMenuItems,                  "The list of items to be supported in the menu window (rmsnxfhualytickw)"),
     OK0()
 };
 
