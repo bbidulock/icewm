147cb47842bbfbbb49b3d42dcae863981fed93d1
Bert Gijsbers <gijsbers@science.uva.nl>
2021-11-13 23:54:55 +0100

    Make DejaVu the default backup font family for corefonts.

--- src/fontmacro.h
+++ src/fontmacro.h
@@ -9,15 +9,21 @@
  && !defined(FONTS_LUCIDA) \
  && !defined(FONTS_DEJAVU) \
  && !defined(FONTS_DROID)
-#define FONTS_ADOBE 1
+#define FONTS_DEJAVU 1
+// #define FONTS_ADOBE 1
 // #define FONTS_LUCIDA 1
-// #define FONTS_DEJAVU 1
 // #define FONTS_DROID 1
 #endif
 
 #define FONT2(pf,px,pt)     pf #px "-" #pt "-*-*-*-*-*-*"       // "iso10646-1"
 
-#if defined(FONTS_ADOBE)
+#if defined(FONTS_DEJAVU)
+#define FONT(px,pt)          FONT2("-misc-dejavu sans-medium-r-normal--", px, *)
+#define BOLDFONT(px,pt)        FONT2("-misc-dejavu sans-bold-r-normal--", px, *)
+#define TTFONT(px,pt)   FONT2("-misc-dejavu sans mono-medium-r-normal--", px, *)
+#define BOLDTTFONT(px,pt) FONT2("-misc-dejavu sans mono-bold-r-normal--", px, *)
+
+#elif defined(FONTS_ADOBE)
 #define FONT(px,pt)   FONT2("-adobe-helvetica-medium-r-normal--", px, pt)
 #define BOLDFONT(px,pt) FONT2("-adobe-helvetica-bold-r-normal--", px, pt)
 #define TTFONT(px,pt)   FONT2("-adobe-courier-medium-r-normal--", px, pt)
@@ -28,12 +34,6 @@
 #define BOLDFONT(px,pt) FONT2("-b&h-lucida-bold-r-normal-*-", px, pt)
 #define TTFONT(px,pt)   FONT2("-b&h-lucidatypewriter-medium-r-normal-*-", px, pt)
 #define BOLDTTFONT(px,pt) FONT2("-b&h-lucidatypewriter-bold-r-normal-*-", px, pt)
-
-#elif defined(FONTS_DEJAVU)
-#define FONT(px,pt)     FONT2("-misc-dejavu sans-medium-r-normal--", px, *)
-#define BOLDFONT(px,pt)   FONT2("-misc-dejavu sans-bold-r-normal--", px, *)
-#define TTFONT(px,pt)   FONT2("-misc-dejavu sans-medium-r-normal--", px, *)
-#define BOLDTTFONT(px,pt) FONT2("-misc-dejavu sans-bold-r-normal--", px, *)
 
 #elif defined(FONTS_DROID)
 #define FONT(px,pt)     FONT2("-misc-droid sans-medium-r-normal--", px, *)
