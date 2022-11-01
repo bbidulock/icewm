2affac139b1a8905c4b9894ff66cf8a0d19bae5a
Bert Gijsbers <gijsbers@science.uva.nl>
2022-11-02 00:11:42 +0100

    Permit a rolled up frame to take focus even if its client has a globally active focus model for issue ice-wm/icewm#98.

--- src/wmmgr.cc
+++ src/wmmgr.cc
@@ -1062,7 +1062,7 @@ void YWindowManager::setFocus(YFrameWindow *f, bool canWarp, bool reorder) {
         else
             w = f->handle();
 
-        if (f->getInputFocusHint())
+        if (f->getInputFocusHint() || (f->isRollup() && !f->avoidFocus()))
             switchFocusTo(f, reorder);
 
         f->setWmUrgency(false);
