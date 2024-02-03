a0c887c7cc85dd278de13159b9b71aebf750017c
Bert Gijsbers <gijsbers@science.uva.nl>
2024-02-03 22:06:07 +0100

    Revert "Force taskbar on XineramaPrimaryScreen" 617b375b79b54602f0848ceff6a654dc7c5b0204.

--- src/wmtaskbar.cc
+++ src/wmtaskbar.cc
@@ -692,10 +692,7 @@ void TaskBar::updateLocation() {
 
     int dx, dy;
     unsigned dw, dh;
-    int screen =
-        inrange(xineramaPrimaryScreen, 0, desktop->getScreenCount() - 1)
-        ? xineramaPrimaryScreen : -1;
-    desktop->getScreenGeometry(&dx, &dy, &dw, &dh, screen);
+    desktop->getScreenGeometry(&dx, &dy, &dw, &dh, -1);
 
     int x = dx;
     unsigned int w = 0;
