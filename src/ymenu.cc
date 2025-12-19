0345ca7e6a3fba927429de0f3de45a5691c513ae
Eduard Bloch <blade@debian.org>
2025-12-19 12:18:40 +0100

    Make menu arrow size relative to icon size
    For https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1108912

--- src/ymenu.cc
+++ src/ymenu.cc
@@ -1205,14 +1205,18 @@ void YMenu::paintItem(Graphics &g, const int i, const int l, const int t,
                         cascadePos + 1, t + top + pad + ih);
             delta = (delta && active);
         }
+
+        // roughly similar size to the old defaults
+        constexpr float afac = 0.65;
+
         if (wmLook == lookGtk || wmLook == lookMotif) {
-            int asize = 9;
+            int asize = ((float) mitem->getIconWidth() * afac);
             int ax = delta + width() - r - 1 - asize * 3 / 2;
             int ay = delta + t + top + pad + (ih - asize) / 2;
             g.setColor(menuBg);
             g.drawArrow(Right, ax, ay, asize, active);
         } else {
-            int asize = 9;
+            int asize = ((float) mitem->getIconWidth() * afac);
             int ax = width() - r - 1 - asize;
             int ay = delta + t + top + pad + (ih - asize) / 2;
             g.setColor(fg);
