4274659492b51836d30befa84f6288d4a8361f5d
Bert Gijsbers <gijsbers@science.uva.nl>
2026-08-03 23:22:53 +0200

    Revert "Support QuickSwitchPreview when _XROOTPMAP_ID is undefined."

--- src/preview.cc
+++ src/preview.cc
@@ -417,26 +417,24 @@ bool SwitchPreview::backcolor() {
 void SwitchPreview::backdrop() {
     manager->grabServer();
     YProperty rootprop(desktop, _XA_XROOTPMAP_ID, F32, 1, XA_PIXMAP);
-    discard();
-    Picture rootPict = None;
     if (rootprop == false) {
-        rootPixmap = None;
-        rootPict = desktop->createPicture();
+        printf("no XROOTPMAP\n");
     }
     else if (rootPixmap != (unsigned long) *rootprop) {
+        discard();
         rootPixmap = (unsigned long) *rootprop;
-        rootPict = xapp->createPicture(rootPixmap, desktop->format());
-    }
-    backPixmap = createPixmap(width(), height());
-    Picture backPict = xapp->createPicture(backPixmap, format());
-    XRenderComposite(xapp->display(), PictOpSrc, rootPict, None, backPict,
-                     x(), y(), 0, 0, 0, 0, width(), height());
+        Picture rootPict = xapp->createPicture(rootPixmap, desktop->format());
+        backPixmap = createPixmap(width(), height());
+        Picture backPict = xapp->createPicture(backPixmap, format());
+        XRenderComposite(xapp->display(), PictOpSrc, rootPict, None, backPict,
+                         x(), y(), 0, 0, 0, 0, width(), height());
 
-    setBackgroundPixmap(backPixmap);
-    clearWindow();
-    xapp->sync();
-    xapp->freePicture(rootPict);
-    xapp->freePicture(backPict);
+        setBackgroundPixmap(backPixmap);
+        clearWindow();
+        xapp->sync();
+        xapp->freePicture(rootPict);
+        xapp->freePicture(backPict);
+    }
     manager->ungrabServer();
 
     int prevWidth = previewWidth();
